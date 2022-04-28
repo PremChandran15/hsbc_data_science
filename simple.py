62ddc569a08617e43780937a66ea42ce89095037
Premkumar Chandrashegaran <premkumar.chandrashegaran@gmail.com>
2022-04-28 11:35:55 +0800

    added some additional notes

--- simple.py
+++ simple.py
@@ -1,4 +1,8 @@
 # This is a very simple python file that prints hello world!. BTW, # in Python means to comment code block
 
 print("hello world!")
-print("This is my first python code")
+print("This is my first")
+
+x = 5/3
+
+print("this is a variable {}".format(x))
